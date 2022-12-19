fcd0f7c8673693dc4c1fd19849ca2e616e23a703
animek12 <111511065+animek12@users.noreply.github.com>
2022-12-19 17:21:41 +0800

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -18,21 +18,21 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //=================================================//
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/c/@ZallMods' // ubah aia
+global.youtube = 'https://youtube.com/c/@alwanganzofficial7125' // ubah aia
 global.ig = '' // ubah aja
-global.email = 'zallmods992@gmail.com'
+global.email = 'notfound@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = '𝐉𝚫𝐍𝐒𝚵𝐍 𝐌Ṏ𝐃𝐒' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = '𝐉𝚫𝐍𝐒𝚵𝐍 𝐌Ṏ𝐃𝐒' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = '𝐉𝚫𝐍𝐒𝚵𝐍 𝐌Ṏ𝐃𝐒' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Xient' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'krl Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Xient' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6283195366404','6283833031795','6283877604686','62895330006855'] // ubah aja pake nomor lu
-global.premium = ['6283195366404'] // ubah aja pake nomor lu
-global.packname = '𝐉𝚫𝐍𝐒𝚵𝐍 𝐌Ṏ𝐃𝐒' // ubah aja
-global.ownerr = ['𝐉𝚫𝐍𝐒𝚵𝐍 𝐌Ṏ𝐃𝐒'] // ubaha aja
-global.author = '𝐉𝚫𝐍𝐒𝚵𝐍 𝐌Ṏ𝐃𝐒' //ubah aja
+global.owner = ['6281349622410','6281349622410','6281349622410','6281349622410'] // ubah aja pake nomor lu
+global.premium = ['6281349622410'] // ubah aja pake nomor lu
+global.packname = 'krl Md' // ubah aja
+global.ownerr = ['Xient'] // ubaha aja
+global.author = 'Xient' //ubah aja
 global.sessionName = './sessionye/haikal' //Gausaj Di Ubah
 global.prefa = ['','!','.',',','🐤','🗿'] // Gausah Mendingan 
 global.sp = '⭔' // Gausah Juga
